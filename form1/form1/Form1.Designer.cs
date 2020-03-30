9947a978789b93c6e19bda1655233cfe9ed6c269
Atuan O <crusifixsso@gmail.com>
2020-03-30 09:25:08 +0100

    Add files via upload

---
+++ form1/form1/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace form1
+{
+    partial class conv_btn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.amount_txt = new System.Windows.Forms.TextBox();
+            this.fromcombo1 = new System.Windows.Forms.ComboBox();
+            this.tocomobo2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Amount";
+            // 
+            // amount_txt
+            // 
+            this.amount_txt.Location = new System.Drawing.Point(125, 107);
+            this.amount_txt.Name = "amount_txt";
+            this.amount_txt.Size = new System.Drawing.Size(121, 20);
+            this.amount_txt.TabIndex = 1;
+            this.amount_txt.TextChanged += new System.EventHandler(this.amount_txt_TextChanged);
+            // 
+            // fromcombo1
+            // 
+            this.fromcombo1.FormattingEnabled = true;
+            this.fromcombo1.Items.AddRange(new object[] {
+            "GBP",
+            "USB",
+            "EUR",
+            "AUD"});
+            this.fromcombo1.Location = new System.Drawing.Point(125, 136);
+            this.fromcombo1.Name = "fromcombo1";
+            this.fromcombo1.Size = new System.Drawing.Size(121, 21);
+            this.fromcombo1.TabIndex = 2;
+            this.fromcombo1.SelectedIndexChanged += new System.EventHandler(this.fromcombo1_SelectedIndexChanged);
+            // 
+            // tocomobo2
+            // 
+            this.tocomobo2.FormattingEnabled = true;
+            this.tocomobo2.Items.AddRange(new object[] {
+            "GBP",
+            "USD",
+            "EUR",
+            "AUD"});
+            this.tocomobo2.Location = new System.Drawing.Point(125, 163);
+            this.tocomobo2.Name = "tocomobo2";
+            this.tocomobo2.Size = new System.Drawing.Size(121, 21);
+            this.tocomobo2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Convert From";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Convert To";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(96, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Converted Amount :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(262, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(51, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "GBP/USD = 1.2361";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(51, 39);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "GBP/AUD = 2.0132";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(51, 62);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(102, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "GBP/EUR = 1.1240";
+            // 
+            // conv_btn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 306);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tocomobo2);
+            this.Controls.Add(this.fromcombo1);
+            this.Controls.Add(this.amount_txt);
+            this.Controls.Add(this.label1);
+            this.Name = "conv_btn";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FX Currency Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox amount_txt;
+        private System.Windows.Forms.ComboBox fromcombo1;
+        private System.Windows.Forms.ComboBox tocomobo2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
